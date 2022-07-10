1cbaeeeb2f4001fc99a9a35ddafaa70d11d9b7f2
Lucero Valdez <lvaldezd.isc91@gmail.com>
2022-07-10 01:08:35 -0500

    Change name project for numbers

--- 3-CrudJFrame/src/crudSLQServer/lvd/views/viewMain.java
+++ 3-CrudJFrame/src/crudSLQServer/lvd/views/viewMain.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Window.Type;
+import java.awt.Font;
 
 public class viewMain extends JFrame {
 
@@ -31,8 +33,10 @@ public class viewMain extends JFrame {
 	 * Create the frame.
 	 */
 	public viewMain() {
+		setFont(new Font("Arial", Font.PLAIN, 14));
+		setType(Type.UTILITY);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1017, 602);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
